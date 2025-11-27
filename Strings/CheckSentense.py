57a1777bc03d191607a18cde9a98a52a064cdaa8
chandu komati <chandukomati135@gmail.com>
2025-11-27 22:56:41 +0530

    Day-29

---
+++ Strings/CheckSentense.py
@@ -0,0 +1,17 @@
+# Check if the Sentence Is Pangram
+
+'''A pangram is a sentence where every letter of the English alphabet appears at least once.
+
+Given a string sentence containing only lowercase English letters, return true if sentence is a pangram, or false otherwise.'''
+
+sentence = "thequickbrownfoxjumpsoverthelazydog"
+alphabets="abcdefghijklmnopqrstuvwxyz"
+flag=False
+if len(sentence)>=26:
+    for i in alphabets:
+        if i in sentence:
+            flag=True
+        else:
+            flag=False
+            break
+print(flag)
