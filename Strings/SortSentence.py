be36a8643ea4cf24f287886bccd0ea23bb60fc30
chandu komati <chandukomati135@gmail.com>
2025-12-02 18:43:55 +0530

    Day-33

--- Strings/SortSentence.py
+++ Strings/SortSentence.py
@@ -7,11 +7,23 @@ A sentence can be shuffled by appending the 1-indexed word position to each word
 For example, the sentence "This is a sentence" can be shuffled as "sentence4 a3 is2 This1" or "is2 sentence4 This1 a3".
 Given a shuffled sentence s containing no more than 9 words, reconstruct and return the original sentence.'''
 
+# s = "is2 sentence4 This1 a3"
+# a=s.split()
+# l=[]
+# for j in range(1,len(a)+1):
+#     for i in range(len(a)):
+#         if str(j) in a[i]:
+#             l.append(a[i].replace(str(j),''))
+# print(" ".join(l))
+
 s = "is2 sentence4 This1 a3"
-a=s.split()
-l=[]
-for j in range(1,len(a)+1):
-    for i in range(len(a)):
-        if str(j) in a[i]:
-            l.append(a[i].replace(str(j),''))
-print(" ".join(l))
+parts = s.split()           # split shuffled words
+n = len(parts)
+out = [""] * n              # prepared list of length n
+
+for p in parts:
+    pos = int(p[-1]) - 1    # 1-indexed position -> 0-indexed
+    word = p[:-1]           # remove the trailing digit
+    out[pos] = word
+
+print(" ".join(out))
