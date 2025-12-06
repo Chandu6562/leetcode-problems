7fbfb00a67cb5cca5d94c130054e692458e236ec
chandu komati <chandukomati135@gmail.com>
2025-12-06 23:12:44 +0530

    Day-35

---
+++ Strings/RepeatedString.py
@@ -0,0 +1,13 @@
+# Repeated Substring Pattern
+'''Given a string s, check if it can be constructed by taking a substring of it and appending multiple copies of the substring together.'''
+
+s = "abcabcabcbc"
+n = len(s)
+is_repeated = False
+for L in range(1, n):  
+    if n % L == 0:    
+        sub = s[:L]
+        if sub * (n // L) == s:
+            is_repeated = True
+            break
+print(is_repeated)
