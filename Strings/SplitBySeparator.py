2c2369a65fe56ac14499872c7890c61f8c23c831
chandu komati <chandukomati135@gmail.com>
2025-12-15 17:57:11 +0530

    Day-41

---
+++ Strings/SplitBySeparator.py
@@ -0,0 +1,22 @@
+# Split Strings by Separator
+'''Given an array of strings words and a character separator, split each string in words by separator.
+
+Return an array of strings containing the new strings formed after the splits, excluding empty strings.
+
+Notes
+
+separator is used to determine where the split should occur, but it is not included as part of the resulting strings.
+A split may result in more than two strings.
+The resulting strings must maintain the same order as they were initially given.'''
+
+# words = ["one.two.three","four.five","six"]
+# separator = "."
+words = ["$easy$","$problem$"]
+separator = "$"
+res=[]
+for i in words:
+    word = i.split(separator)
+    for j in word:
+        if len(j)!=0:
+            res.append(j)
+print(res)
