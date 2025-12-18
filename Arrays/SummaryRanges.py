87bfbea3d1bce56d69e30d9d3f4ed90db308f384
chandu komati <chandukomati135@gmail.com>
2025-12-18 22:30:04 +0530

    Day-42

---
+++ Arrays/SummaryRanges.py
@@ -0,0 +1,43 @@
+# Summary Ranges
+'''You are given a sorted unique integer array nums.
+
+A range [a,b] is the set of all integers from a to b (inclusive).
+
+Return the smallest sorted list of ranges that cover all the numbers in the array exactly.
+That is, each element of nums is covered by exactly one of the ranges, 
+and there is no integer x such that x is in one of the ranges but not in nums.
+
+Each range [a,b] in the list should be output as:
+
+"a->b" if a != b
+"a" if a == b'''
+
+
+def summaryRanges(nums):
+    result = []
+    if not nums:
+        return result
+
+    start = nums[0]
+
+    for i in range(1, len(nums)):
+        if nums[i] != nums[i - 1] + 1:
+            end = nums[i - 1]
+            if start == end:
+                result.append(str(start))
+            else:
+                result.append(f"{start}->{end}")
+            start = nums[i]
+
+    # Add the last range
+    end = nums[-1]
+    if start == end:
+        result.append(str(start))
+    else:
+        result.append(f"{start}->{end}")
+
+    return result
+nums = [0,1,2,4,5,7]
+print(summaryRanges(nums))
+
+
