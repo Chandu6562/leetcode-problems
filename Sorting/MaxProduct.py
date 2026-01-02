ba0eb842cdabb2741203e28f038ef536256e50e1
chandu komati <chandukomati135@gmail.com>
2026-01-02 23:10:29 +0530

    Day-47

--- Sorting/MaxProduct.py
+++ Sorting/MaxProduct.py
@@ -2,11 +2,17 @@
 '''Given the array of integers nums, you will choose two different indices i and j of that array. 
 Return the maximum value of (nums[i]-1)*(nums[j]-1).'''
 
+# nums = [3,4,5,2]
+# res=0
+# for i in range(len(nums)):
+#     for j in range(i+1,len(nums)):
+#         a=nums[i]-1
+#         b=nums[j]-1
+#         res=max(a*b,res)
+# print(res)
+
 nums = [3,4,5,2]
-res=0
-for i in range(len(nums)):
-    for j in range(i+1,len(nums)):
-        a=nums[i]-1
-        b=nums[j]-1
-        res=max(a*b,res)
-print(res)
+nums.sort()
+a=nums[-1]-1
+b=nums[-2]-1
+print(a*b)
