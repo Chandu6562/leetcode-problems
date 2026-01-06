687bf823e4608657e0fdb06b54464ce7ec46e8d3
chandu komati <chandukomati135@gmail.com>
2026-01-06 21:57:54 +0530

    Day-49

---
+++ Strings/CheckCharacters.py
@@ -0,0 +1,13 @@
+# Check if All Characters Have Equal Number of Occurrences
+'''Given a string s, return true if s is a good string, or false otherwise.
+
+A string s is good if all the characters that appear in s have the same number of occurrences (i.e., the same frequency).'''
+
+s = "abacbc"
+d={}
+for i in s:
+    if i not in d:
+        d[i]=1
+    else:
+        d[i]+=1
+print(len(set(d.values()))==1)
