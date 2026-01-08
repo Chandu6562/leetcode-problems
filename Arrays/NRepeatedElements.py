d84cf63a3d79f6e3899d8352c931d07c218bdc1b
chandu komati <chandukomati135@gmail.com>
2026-01-08 18:55:27 +0530

    Day-49

--- Arrays/NRepeatedElements.py
+++ Arrays/NRepeatedElements.py
@@ -6,7 +6,8 @@ nums contains n + 1 unique elements.
 Exactly one element of nums is repeated n times.
 Return the element that is repeated n times.'''
 
-nums = [5,1,5,2,5,3,5,4]
+# nums = [5,1,5,2,5,3,5,4]
+nums = [2,1,2,5,3,2]
 n=len(nums)//2
 res=0
 for i in nums:
