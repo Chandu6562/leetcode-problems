0b3eb95bf55d4b48cc2b112c4cd7df12efb7d77b
chandu komati <chandukomati135@gmail.com>
2026-01-24 20:11:31 +0530

    Day-50

---
+++ Hash Table/UniqueNoOfOccuenences.py
@@ -0,0 +1,15 @@
+# Unique Number of Occurrences
+'''Given an array of integers arr, return true if the number of occurrences of each value in the array is unique or false otherwise.'''
+
+arr = [1,2,2,1,1,3]
+d={}
+for i in arr:
+    if i in d:
+        d[i]+=1
+    else:
+        d[i]=1
+l=[]
+for j in d.values():
+    if j not in l:
+        l.append(j)
+print(len(d)==len(l))
