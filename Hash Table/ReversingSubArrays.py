e79e7900edafc2d42ab02d905d6924d30cc7045d
chandu komati <chandukomati135@gmail.com>
2026-01-27 21:40:13 +0530

    Day-51

--- Hash Table/ReversingSubArrays.py
+++ Hash Table/ReversingSubArrays.py
@@ -4,8 +4,10 @@ you can select any non-empty subarray of arr and reverse it. You are allowed to 
 
 Return true if you can make arr equal to target or false otherwise.'''
 
-target = [3,7,9]
-arr = [3,7,11]
+# target = [3,7,9]
+# arr = [3,7,11]
+target = [1,2,3,4]
+arr = [2,4,1,3]
 if arr == target:
     print('True')
 else:
