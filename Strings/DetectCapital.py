c2f26342e23ccb4c0efa9b78b1c3cabed0cb6e48
chandu komati <chandukomati135@gmail.com>
2026-02-13 23:07:16 +0530

    Detect Capital

---
+++ Strings/DetectCapital.py
@@ -0,0 +1,17 @@
+# Detect Capital
+'''Detect CapitalWe define the usage of capitals in a word to be right when one of the following cases holds:
+
+All letters in this word are capitals, like "USA".
+All letters in this word are not capitals, like "leetcode".
+Only the first letter in this word is capital, like "Google".
+Given a string word, return true if the usage of capitals in it is right.'''
+
+word = "fdhD"
+if word.isupper():
+    print("True")
+elif word.islower():
+    print("True")
+elif word.istitle():
+    print("True")
+else:
+    print("False")
