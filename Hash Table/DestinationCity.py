9f7e9158f5acaf55d424a9c16f7c4e2f719d13af
chandu komati <chandukomati135@gmail.com>
2026-02-20 12:43:32 +0530

    Destination City

---
+++ Hash Table/DestinationCity.py
@@ -0,0 +1,16 @@
+# estination City
+
+'''You are given the array paths, where paths[i] = [cityAi, cityBi] means there exists a direct path going from cityAi to cityBi.
+Return the destination city, that is, the city without any path outgoing to another city.
+
+It is guaranteed that the graph of paths forms a line without any loop, therefore, there will be exactly one destination city.'''
+
+paths = [["London","New York"],["New York","Lima"],["Lima","Sao Paulo"]]
+
+starting_city = set()
+for cityA,cityB in paths:
+    starting_city.add(cityA)
+
+for cityA, cityB in paths:
+    if cityB not in starting_city:
+        print(cityB)
