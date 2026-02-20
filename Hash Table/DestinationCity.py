aad4754511266120e14d709b58c33651c38990c6
chandu komati <chandukomati135@gmail.com>
2026-02-20 12:45:40 +0530

    Destination City

--- Hash Table/DestinationCity.py
+++ Hash Table/DestinationCity.py
@@ -5,7 +5,8 @@ Return the destination city, that is, the city without any path outgoing to anot
 
 It is guaranteed that the graph of paths forms a line without any loop, therefore, there will be exactly one destination city.'''
 
-paths = [["London","New York"],["New York","Lima"],["Lima","Sao Paulo"]]
+# paths = [["London","New York"],["New York","Lima"],["Lima","Sao Paulo"]]
+paths = [["B","C"],["D","B"],["C","A"]]
 
 starting_city = set()
 for cityA,cityB in paths:
